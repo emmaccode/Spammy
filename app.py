74e630185668f26010d62ba7486cbab7bdefc01b
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-11-02 12:01:45 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import flask
+import json
+from flask import Flask, render_template, request
+from joblib import load
+import pandas as pd
+# <--- Deps --->
+# (Main)
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/')
+def template():
+    try:
+        bathrooms = request.args['bathrooms']
+        bedrooms = request.args['bedrooms']
+        squarefeet = request.args['squarefeet']
+        yearbuilt = request.args['yearbuilt']
+    except KeyError as e:
+        return ('Some Values are missing')
+    try:
+        bathrooms = float(bathrooms)
+        bedrooms = float(bedrooms)
+        squarefeet = float(squarefeet)
+        yearbuilt = int(yearbuilt)
+    except ValueError as e:
+        return ('That aint a number, Cowboy.')
+    else:
+        dcry = pd.DataFrame({"YearBuilt": [yearbuilt],
+        "LotSize": [squarefeet],"Bedrooms": [bedrooms],
+        "Bathrooms": [bathrooms]})
+        pipeline = load('/ML/alg.sav')
+        estimate = pipeline.predict(dcry)
+        return str(int(estimate))
+
+if __name__ == '__main__':
+    app.run(debug=False)
